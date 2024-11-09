18df7240f2e0adbd8df580606a0b26972a14c186
PankajVeerma <pankajverma3242@gmail.com>
2024-11-09 21:58:33 +0530

    Weather APp

---
+++ wheatherAPI.py
@@ -0,0 +1,15 @@
+import requests, json
+import datetime
+api_key = "516e5492d88e639900a36168fff7d4e6"
+base_url = "https://api.openweathermap.org/data/2.5/weather?q="
+city = input("Enter city Name : ")
+complete_Url= base_url + city  + "&appid="  + api_key
+response =requests.get(complete_Url)
+data_formate  = response.json()
+kelvin = data_formate["main"]["temp"]
+Celsius = (kelvin - 273.15 ) 
+humidity = data_formate["main"]["humidity"]
+vist = data_formate["visibility"]
+dist = data_formate["weather"][0]["description"]
+
+print(f"The current weather Report the tempearture is {Celsius }celsius, humidity is {humidity} visibility is {vist}  and the  \n{dist} ")
